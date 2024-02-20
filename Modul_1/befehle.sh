c7f5dcaf6dea60421216f8aca0c77c99a466a2af
balindstrot <62093098+balindstrot@users.noreply.github.com>
2024-02-20 15:23:05 +0100

    Delete Modul_1 directory

--- Modul_1/befehle.sh
+++
@@ -1,2 +0,0 @@
-sed -e '/^#/d' -e '/IMPOTANT/d' 2023-11-20-Article_list_dirty.txt | cut -f5,12 2023-11-20-Article_list_dirty.txt | sed -e '/^\s/d' -e '/eng/d' -e '/^#/d' -e '/ISSN:/d' -e '/issn:\s/d' -e '/Issn:\s*/d' | sort -n | uniq > 2023-11-20-Dates_and_ISSNs.tsv
-
